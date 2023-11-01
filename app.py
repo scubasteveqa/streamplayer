11ab772bff0698a7a0276f711c75c7c94b8cd5f6
scubastevew77 <147108604+scubastevew77@users.noreply.github.com>
2023-11-01 10:10:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from streamlit_player import st_player
 
 # Embed a youtube video
-st_player("https://youtu.be/CmSKVW1v0xM")
+st_player("https://www.youtube.com/watch?v=eBGIQ7ZuuiU")
 
 # Embed a music from SoundCloud
 st_player("https://soundcloud.com/imaginedragons/demons")
