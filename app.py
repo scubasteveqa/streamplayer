87151894cad3c40b1bfd66378a7408a3471f5a19
scubastevew77 <147108604+scubastevew77@users.noreply.github.com>
2023-12-14 11:34:52 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from streamlit_player import st_player
 
 # Embed a youtube video Rick Roll
-#st_player("https://www.youtube.com/watch?v=eBGIQ7ZuuiU")
+st_player("https://www.youtube.com/watch?v=eBGIQ7ZuuiU")
 
 # Embed a youtube video - Alan Walker
 st_player("https://www.youtube.com/watch?v=wJnBTPUQS5A")
 
 # Embed a music from SoundCloud
-#st_player("https://soundcloud.com/imaginedragons/demons")
+st_player("https://soundcloud.com/imaginedragons/demons")
